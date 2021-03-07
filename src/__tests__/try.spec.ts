33a61154b6dbbb395215a8757d9f88e0e70bf9de
Thomas Reynolds <me@tdreyno.com>
2021-03-07 03:20:07 +0000

    fix: simpler PIITry

--- src/__tests__/try.spec.ts
+++ src/__tests__/try.spec.ts
@@ -1,23 +1,18 @@
 import { PIITry, unwrap } from "../index"
-import { identity } from "../result"
 
 describe("PIITry", () => {
   it("should wrap results in PII", async () => {
     const result = await PIITry(() => "TEST_STRING")
-    const success = result.fold(identity, () => void 0)
-    expect(unwrap(success)).toBe("TEST_STRING")
+    expect(result).toBe("TEST_STRING")
   })
 
   it("should wrap exceptions in PII", async () => {
-    const result = await PIITry<never, Error>(() => {
-      throw new Error("Error message")
-    })
-
-    const errorMessage = result.fold(
-      () => "never",
-      e => unwrap(e).message,
-    )
-
-    expect(errorMessage).toBe("Error message")
+    try {
+      await PIITry(() => {
+        throw new Error("Error message")
+      })
+    } catch (e) {
+      expect(unwrap(e).message).toBe("Error message")
+    }
   })
 })
